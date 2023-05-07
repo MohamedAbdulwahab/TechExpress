53559dfc82b2078a4cbbb1a470453b0d28d42aa7
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-07 08:41:28 -0700

    added the ability to display a counter for cart items

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,14 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const itemsInCart = cartItems.reduce((acc, currentItem) => {
+    return acc + Number(currentItem.quantity);
+  }, 0);
+
   return (
     <header>
       <Navbar collapseOnSelect bg='dark' variant='dark' expand='lg'>
@@ -13,7 +20,10 @@ function Header() {
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='ms-auto'>
               <Nav.Link as={Link} to='/cart'>
-                <i className='fa fa-shopping-cart'></i> Cart
+                <i className='fa fa-shopping-cart'></i> Cart{' '}
+                {cartItems.length > 0 && (
+                  <Badge bg='primary'>{itemsInCart}</Badge>
+                )}
               </Nav.Link>
               <Nav.Link as={Link} to='/login'>
                 <i className='fa fa-user'></i> Sign In
