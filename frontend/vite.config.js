58e5d1afb89697d1a767ed92796fe088bb4da5a0
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-20 12:09:31 -0700

    added proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // proxy settings
+  server: {
+    proxy: {
+      '/api/products': {
+        target: 'http://localhost:5500',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
