1e338a5ea6e57e3cb613ed06a2b76d3ab372cacb
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-21 02:12:25 -0700

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
-import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.get('/api/products', (req, res) => {
   res.json(products);
@@ -18,10 +21,10 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const environment = process.env.NODE_ENV || 'development';
-const PORT = process.env.PORT || 5500;
-
+const PORT = process.env.PORT;
 app.listen(
   PORT,
-  console.log(`server is running in ${environment} mode on port ${PORT}`)
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 );
