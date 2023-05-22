4c8e8c0858bfe8e9cca9d661e8249754fe2e49ab
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-22 12:15:42 -0700

    added custom error handlers

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,21 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 /* .env file must be located in root folder */
 dotenv.config();
 
 const app = express();
 
+/* connect to Mongo database */
 connectDB();
 
 app.use('/api/products', productRoutes);
+
+/* custom error handlers */
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(
