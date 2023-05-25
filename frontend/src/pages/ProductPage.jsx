f3f2f42b7389408dafa600d444224325c1003c11
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-25 10:31:06 -0700

    added navigation on addToCart

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -7,13 +7,20 @@ import {
   Button,
   ListGroup,
   ListGroupItem,
+  Form,
 } from 'react-bootstrap';
 import Ratings from '../components/Ratings';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchSingleProduct } from '../api/products.js';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useState } from 'react';
 
 function ProductPage() {
+  // state to track the quantity
+  const [quantity, setQuantity] = useState(1);
+
   // get the parameter passed from thr path in the App component.
   const { userId } = useParams();
 
@@ -32,10 +39,14 @@ function ProductPage() {
   });
 
   if (isLoading) {
-    return 'Data is loading...';
+    return <Loader />;
   } else if (isError) {
-    return `Error: ${error}`;
+    return <Message variant='danger'>Error: {error.message}</Message>;
   }
+
+  const handleAddToCart = () => {
+    navigate(`/cart/${userId}/qty=${quantity}`);
+  };
 
   return (
     <>
@@ -86,9 +97,39 @@ function ProductPage() {
                   </Row>
                 </ListGroupItem>
 
+                {product.countInStock > 0 && (
+                  <ListGroupItem>
+                    <Row>
+                      <Col>Quantity</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          size='sm'
+                          value={quantity}
+                          onChange={(e) => setQuantity(e.target.value)}
+                        >
+                          <option>Select Quantity</option>
+                          {Array.from(Array(product.countInStock).keys()).map(
+                            (num) => {
+                              return (
+                                <option key={num + 1} value={num + 1}>
+                                  {num + 1}
+                                </option>
+                              );
+                            }
+                          )}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
+                )}
+
                 <ListGroupItem>
                   <Row>
-                    <Button disabled={product.countInStock === 0}>
+                    <Button
+                      disabled={product.countInStock === 0}
+                      onClick={handleAddToCart}
+                    >
                       Add to Cart
                     </Button>
                   </Row>
