0e37aecfee67cecf2a535a2049fceb0ad06d3a1e
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-28 10:26:57 -0700

    added sessions and passport

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,52 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
+import session from 'express-session';
+import passport from 'passport';
+import configurePassport from './config/passport.js';
+import MongoStore from 'connect-mongo';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 /* .env file must be located in root folder */
 dotenv.config();
 
 const app = express();
 
+/* Passport config */
+configurePassport(passport);
+
 /* connect to Mongo database */
 connectDB();
 
+// Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+/* Passport middleware */
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
 app.use('/api/products', productRoutes);
 
 /* custom error handlers */
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5500;
 app.listen(
   PORT,
   console.log(
