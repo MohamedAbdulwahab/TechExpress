27d36c3fe601cac55d535a3db847a12b6f60e4f3
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-31 01:48:11 -0700

    added redux toolkit Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '../src/index.css';
 import '../src/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -12,8 +14,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
