7958ec56edce2386927beb922403daf1811fcb97
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-05-31 01:49:27 -0700

    removed react query Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,19 +5,11 @@ import '../src/index.css';
 import '../src/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-// create React Query client
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
