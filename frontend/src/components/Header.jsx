2868577bc2975cdf1e62819201b0552bc16dbb5b
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-06 00:26:42 -0700

    added dynamic header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,31 @@
-import { Container, Nav, Navbar, Badge } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Container, Nav, Navbar, Badge, NavDropdown } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../store/slices/authSlice';
+import { useLogoutMutation } from '../store/slices/usersApiSlice';
 
 function Header() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.login);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
 
   const itemsInCart = cartItems.reduce((acc, currentItem) => {
     return acc + Number(currentItem.quantity);
   }, 0);
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <header>
@@ -19,15 +37,29 @@ function Header() {
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='ms-auto'>
+              {/* cart  */}
               <Nav.Link as={Link} to='/cart'>
                 <i className='fa fa-shopping-cart'></i> Cart{' '}
                 {cartItems.length > 0 && (
                   <Badge bg='primary'>{itemsInCart}</Badge>
                 )}
               </Nav.Link>
-              <Nav.Link as={Link} to='/login'>
-                <i className='fa fa-user'></i> Sign In
-              </Nav.Link>
+
+              {/* sign in or userInfo */}
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown.Item as={Link} to='/profile'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
+                  <i className='fa fa-user'></i> Sign In
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
