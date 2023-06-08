49c2b5d708408ecc15f6656a36342cb81394a522
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-08 13:08:50 -0700

    added private route for payment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './pages/HomeScreen';
 import ErrorPage from './pages/ErrorPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import PaymentPage from './pages/PaymentPage';
+import ShippingPage from './pages/ShippingPage';
+import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,12 +19,19 @@ function App() {
       <Header />
       <main>
         <Routes>
+          {/* public Routes  */}
           <Route path='/' element={<HomeScreen />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/cart' element={<CartPage />} />
           <Route path='/product/:id' element={<ProductPage />} />
           <Route path='*' element={<ErrorPage />} />
+
+          {/* private/protected routes  */}
+          <Route path='' element={<PrivateRoute />}>
+            <Route path='/shipping' element={<ShippingPage />} />
+            <Route path='/payment' element={<PaymentPage />} />
+          </Route>
         </Routes>
       </main>
       <Footer />
