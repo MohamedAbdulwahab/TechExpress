0f1c013f407bfe4a10d92f446d93fda20bd0bcb6
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-11 14:35:58 -0700

    added paypal provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '../src/index.css';
 import '../src/bootstrap.min.css';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+
+const initialOptions = {
+  clientId: 'test',
+  currency: 'USD',
+  intent: 'capture',
+  components: 'buttons',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PayPalScriptProvider deferLoading={false} options={initialOptions}>
+        <App />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
