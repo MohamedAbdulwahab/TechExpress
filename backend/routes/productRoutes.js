9e73e761b32ab1400d6f6fa5e06bcc1247b73d9c
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-17 14:44:14 -0700

    added routes to create and delete products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,18 @@ import express from 'express';
 import {
   getProducts,
   getProductById,
+  createProduct,
+  deleteProductById,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProductById);
 
 export default router;
