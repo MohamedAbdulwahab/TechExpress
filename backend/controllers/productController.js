5a41f08725d7d8ac52c2fc5a038595f0512c448b
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-18 14:46:53 -0700

    added controllers to create and delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 // @desc    Fetch all products
@@ -30,4 +31,48 @@ const getProductById = async (req, res) => {
   }
 };
 
-export { getProducts, getProductById };
+// @desc    Create a new product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  // create a new product.
+  const product = await Product.create({
+    name: 'sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/airpods.jpg',
+    brand: 'sample brand',
+    category: 'sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'sample discription',
+  });
+
+  if (product) {
+    // response with newly created product in json format.
+    res.status(200).json(product);
+  } else {
+    res.status(404).json({ message: 'Failed to create a new product' });
+    throw new Error('Failed to create a new product');
+  }
+});
+
+// @desc    Create a new product
+// @route   POST /api/products
+// @access  Private/Admin
+const deleteProductById = asyncHandler(async (req, res) => {
+  // find the product to be deleted.
+  const product = await Product.findById(req.params.id);
+
+  // product exists.
+  if (product) {
+    // response with all products in json format.
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json('product deleted');
+  } else {
+    res.status(404);
+    throw new Error('product not found');
+  }
+});
+
+export { getProducts, getProductById, createProduct, deleteProductById };
