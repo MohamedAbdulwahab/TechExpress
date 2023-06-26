50724bb792af41f2a98bdfbda2b542f30bbd6a30
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-25 22:24:16 -0700

    added pagination

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,12 +1,19 @@
-import { Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import ShowProduct from './ShowProduct';
 import Loader from './Loader';
 import Message from './Message';
+import Paginate from './Pagination';
+import { useParams } from 'react-router-dom';
 import { useGetProductsQuery } from '../store/slices/productsApiSlice';
 
 function Products() {
+  const { pageNumber, keyword } = useParams();
+
   // Redux toolkit (fetch all products)
-  const { data: products, isLoading, isError, error } = useGetProductsQuery();
+  const { data, isLoading, isError, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
 
   if (isLoading) {
     return <Loader />;
@@ -14,15 +21,28 @@ function Products() {
     return <Message variant='danger'>Error: {error.message}</Message>;
   }
 
-  const renderedProducts = products.map((product) => {
+  const renderedProducts = data.products.map((product) => {
     return (
-      <Col key={product._id} md={6} lg={4} lx={3}>
+      <Col key={product._id} md={6} lg={4} xl={4}>
         <ShowProduct product={product} />
       </Col>
     );
   });
 
-  return renderedProducts;
+  return (
+    <>
+      <Row>{renderedProducts}</Row>
+      <Row>
+        <Col className='pagination-center pt-4'>
+          <Paginate
+            numberOfPages={data.numberOfPages}
+            currentPage={data.pageNumber}
+            keyword={keyword ? keyword : ''}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Products;
