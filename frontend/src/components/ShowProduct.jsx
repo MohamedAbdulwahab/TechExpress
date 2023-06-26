aa0394b808431c26434b34815ab0cb624693f595
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-06-25 22:24:51 -0700

    added fluid images

--- frontend/src/components/ShowProduct.jsx
+++ frontend/src/components/ShowProduct.jsx
@@ -6,7 +6,7 @@ function ShowProduct({ product }) {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img variant='top' src={product.image} />
+        <Card.Img variant='top' src={product.image} fluid='true' />
       </Link>
       <Card.Body>
         {/* Title */}
